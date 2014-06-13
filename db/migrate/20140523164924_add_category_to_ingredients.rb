33e27ed62a5b24930b5efd8490561e2608a6910e
David Ortiz <davidortizfrau@gmail.com>
2014-06-12 20:56:10 -0600

    Migration

--- db/migrate/20140523164924_add_category_to_ingredients.rb
+++ db/migrate/20140523164924_add_category_to_ingredients.rb
@@ -2,6 +2,6 @@ class AddCategoryToIngredients < ActiveRecord::Migration
   def change
     add_column :ingredients, :ingredient_category_id, :integer
     add_index :ingredients, :ingredient_category_id
-    remove_column :ingredients, :category, :string
+    remove_column :ingredients, :category
   end
 end
